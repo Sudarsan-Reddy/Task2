26bf026854b18cf2e0bc6399a2fe02eed32023fe
sudarsan reddy <sudarsan.reddy@liferay.com>
2017-05-23 11:46:10 +0530

    task2 folder

---
+++ index.js
@@ -0,0 +1,73 @@
+(function(){
+
+var lessons;
+
+$(document).ready(function() {
+
+$.get( "https://final-project-recording.firebaseio.com/lessons.json", function( data ) {
+
+lessons = Object.values(data);
+
+$("#lessons").html(generateHtml(lessons));
+
+});
+
+$('#btn').on('click',function() {
+
+lessons.forEach(function(lesson, index) {
+lessons[index].description = index + ' - ' + lessons[index].description
+});
+
+var lessonsDiv = document.getElementById('lessons');
+
+// delete all child elements
+while (lessonsDiv.firstChild) {
+lessonsDiv.removeChild(lessonsDiv.firstChild);
+}
+
+// apply the table directly to the DOM
+lessonsDiv.appendChild(generateDomTable(lessons));
+
+});
+});
+
+}());
+
+function generateDomTable(lessons) {
+
+var table = document.createElement('table');
+
+var header = table.createTHead();
+var row = header.insertRow(0);
+var cell = row.insertCell(0);
+cell.innerHTML = "Description";
+
+for (var i = 0; i < lessons.length; i++) {
+
+var row = table.insertRow(i);
+var col = row.insertCell(0);
+
+col.innerHTML = lessons[i].description;
+}
+
+return table;
+}
+
+
+function generateHtml(lessons) {
+var html = "<table class='table lessons-list'>" +
+"<thead>" +
+"<th>Description</th>" +
+"</thead>" +
+"<tbody>";
+
+lessons.forEach(function(lesson) {
+html += '<tr>' +
+'<td>' + lesson.description + '</td>' +
+'</tr>';
+});
+
+html += '</tbody></table>';
+
+return html;
+}
---
+++ renault.js
@@ -0,0 +1,472 @@
+HELIOS.components.c040 = HELIOS.components.c040 || {};
+HELIOS.components.c040.dealers = {
+		"totalResults": 372,
+		"dealers": [{
+					"id": "in_renault_35607554-004",
+					"dealerId": "35607554",
+					"tradingName": "RENAULT AMBALA",
+					"geolocation": {
+						"latitude": 30.304372,
+						"longitude": 76.952826
+					},
+					"suggestedName": "RENAULT AMBALA - AMBALA",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607551-001",
+					"dealerId": "35607551",
+					"tradingName": "RENAULT AMRITSAR",
+					"geolocation": {
+						"latitude": 31.573827,
+						"longitude": 75.005718
+					},
+					"suggestedName": "RENAULT AMRITSAR - AMRITSAR",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95001301-003",
+					"dealerId": "95001301",
+					"tradingName": "RENAULT Anna Nagar",
+					"geolocation": {
+						"latitude": 13.081568,
+						"longitude": 80.198738
+					},
+					"suggestedName": "RENAULT Anna Nagar - Chennai",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607563-016",
+					"dealerId": "35607563",
+					"tradingName": "RENAULT BARAMATI",
+					"geolocation": {
+						"latitude": 18.184135,
+						"longitude": 74.610763
+					},
+					"suggestedName": "RENAULT BARAMATI - BARAMATI",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607571-006",
+					"dealerId": "35607571",
+					"tradingName": "RENAULT BATHINDA",
+					"geolocation": {
+						"latitude": 30.280682,
+						"longitude": 74.936964
+					},
+					"suggestedName": "RENAULT BATHINDA - BATHINDA",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607476-001",
+					"dealerId": "35607476",
+					"tradingName": "RENAULT BHOPAL",
+					"geolocation": {
+						"latitude": 23.2472,
+						"longitude": 77.4444
+					},
+					"suggestedName": "RENAULT BHOPAL - Bhopal",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607477-002",
+					"dealerId": "35607477",
+					"tradingName": "RENAULT BHOPAL",
+					"geolocation": {
+						"latitude": 23.3029,
+						"longitude": 77.4038
+					},
+					"suggestedName": "RENAULT BHOPAL - Bhopal",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607576-001",
+					"dealerId": "35607576",
+					"tradingName": "RENAULT BOKARO",
+					"geolocation": {
+						"latitude": 23.665827,
+						"longitude": 86.151521
+					},
+					"suggestedName": "RENAULT BOKARO - BOKARO",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607557-005",
+					"dealerId": "35607557",
+					"tradingName": "RENAULT BULANDSHAHR",
+					"geolocation": {
+						"latitude": 28.402327,
+						"longitude": 77.828741
+					},
+					"suggestedName": "RENAULT BULANDSHAHR - BULANDSHAHR",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_20005963-001",
+					"dealerId": "20005963",
+					"tradingName": "RENAULT Banjara Hills",
+					"geolocation": {
+						"latitude": 17.424971,
+						"longitude": 78.42186
+					},
+					"suggestedName": "RENAULT Banjara Hills - Hyderabad",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95002620-002",
+					"dealerId": "95002620",
+					"tradingName": "RENAULT Bhilai",
+					"geolocation": {
+						"latitude": 21.210018,
+						"longitude": 81.373329
+					},
+					"suggestedName": "RENAULT Bhilai - Bhilai",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95002600-001",
+					"dealerId": "95002600",
+					"tradingName": "RENAULT Bilaspur",
+					"geolocation": {
+						"latitude": 22.0788,
+						"longitude": 82.140823
+					},
+					"suggestedName": "RENAULT Bilaspur - Bilaspur",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607574-004",
+					"dealerId": "35607574",
+					"tradingName": "RENAULT CHAPRA",
+					"geolocation": {
+						"latitude": 25.779021,
+						"longitude": 84.771172
+					},
+					"suggestedName": "RENAULT CHAPRA - CHAPRA",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607597-002",
+					"dealerId": "35607597",
+					"tradingName": "RENAULT CHARKHI DADRI",
+					"geolocation": {
+						"latitude": 28.593105,
+						"longitude": 76.24855
+					},
+					"suggestedName": "RENAULT CHARKHI DADRI - CHARKHI DADRI",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607579-006",
+					"dealerId": "35607579",
+					"tradingName": "RENAULT CHIPLUN",
+					"geolocation": {
+						"latitude": 17.519175,
+						"longitude": 73.5556
+					},
+					"suggestedName": "RENAULT CHIPLUN - CHIPLUN",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95000560-005",
+					"dealerId": "95000560",
+					"tradingName": "RENAULT Calicut",
+					"geolocation": {
+						"latitude": 11.259293,
+						"longitude": 75.784119
+					},
+					"suggestedName": "RENAULT Calicut - Calicut",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_20005981-001",
+					"dealerId": "20005981",
+					"tradingName": "RENAULT Chandigarh",
+					"geolocation": {
+						"latitude": 30.710405,
+						"longitude": 76.80612
+					},
+					"suggestedName": "RENAULT Chandigarh - Chandigarh",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_20005980-002",
+					"dealerId": "20005980",
+					"tradingName": "RENAULT Chandivali Service",
+					"geolocation": {
+						"latitude": 19.113136,
+						"longitude": 72.890938
+					},
+					"suggestedName": "RENAULT Chandivali Service - Mumbai",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95000260-001",
+					"dealerId": "95000260",
+					"tradingName": "RENAULT Coimbatore",
+					"geolocation": {
+						"latitude": 11.028002,
+						"longitude": 76.951514
+					},
+					"suggestedName": "RENAULT Coimbatore - Coimbatore",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607598-003",
+					"dealerId": "35607598",
+					"tradingName": "RENAULT DEHRADUN BP",
+					"geolocation": {
+						"latitude": 30.266111,
+						"longitude": 77.9825
+					},
+					"suggestedName": "RENAULT DEHRADUN BP - DEHRADUN",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95002780-001",
+					"dealerId": "95002780",
+					"tradingName": "RENAULT Dehradun",
+					"geolocation": {
+						"latitude": 30.318359,
+						"longitude": 78.029022
+					},
+					"suggestedName": "RENAULT Dehradun - Dehradun",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95003180-001",
+					"dealerId": "95003180",
+					"tradingName": "RENAULT Delhi East",
+					"geolocation": {
+						"latitude": 28.639677,
+						"longitude": 77.313486
+					},
+					"suggestedName": "RENAULT Delhi East - Delhi",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95001300-003",
+					"dealerId": "95001300",
+					"tradingName": "RENAULT Delhi North",
+					"geolocation": {
+						"latitude": 28.699924,
+						"longitude": 77.184448
+					},
+					"suggestedName": "RENAULT Delhi North - Delhi",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95001320-003",
+					"dealerId": "95001320",
+					"tradingName": "RENAULT Delhi South",
+					"geolocation": {
+						"latitude": 28.553352,
+						"longitude": 77.242813
+					},
+					"suggestedName": "RENAULT Delhi South - Delhi",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_20005954-001",
+					"dealerId": "20005954",
+					"tradingName": "RENAULT Delhi West",
+					"geolocation": {
+						"latitude": 28.659983,
+						"longitude": 77.147119
+					},
+					"suggestedName": "RENAULT Delhi West - New Delhi",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607594-003",
+					"dealerId": "35607594",
+					"tradingName": "RENAULT GOLAGHAT",
+					"geolocation": {
+						"latitude": 26.523883,
+						"longitude": 93.970203
+					},
+					"suggestedName": "RENAULT GOLAGHAT - GOLAGHAT",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95002740-001",
+					"dealerId": "95002740",
+					"tradingName": "RENAULT Ghaziabad",
+					"geolocation": {
+						"latitude": 28.671009,
+						"longitude": 77.44297
+					},
+					"suggestedName": "RENAULT Ghaziabad - Ghaziabad",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95002700-001",
+					"dealerId": "95002700",
+					"tradingName": "RENAULT Guwahati",
+					"geolocation": {
+						"latitude": 26.135005,
+						"longitude": 91.810039
+					},
+					"suggestedName": "RENAULT Guwahati - Guwahati",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95002720-002",
+					"dealerId": "95002720",
+					"tradingName": "RENAULT Guwahati",
+					"geolocation": {
+						"latitude": 26.186021,
+						"longitude": 91.844577
+					},
+					"suggestedName": "RENAULT Guwahati - Guwahati",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607566-003",
+					"dealerId": "35607566",
+					"tradingName": "RENAULT HALDIA",
+					"geolocation": {
+						"latitude": 22.101587,
+						"longitude": 88.050365
+					},
+					"suggestedName": "RENAULT HALDIA - HALDIA",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607561-002",
+					"dealerId": "35607561",
+					"tradingName": "RENAULT HATHRAS",
+					"geolocation": {
+						"latitude": 27.586473,
+						"longitude": 78.051544
+					},
+					"suggestedName": "RENAULT HATHRAS - HATHRAS",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95002680-001",
+					"dealerId": "95002680",
+					"tradingName": "RENAULT Hubli",
+					"geolocation": {
+						"latitude": 15.367721,
+						"longitude": 75.120923
+					},
+					"suggestedName": "RENAULT Hubli - Hubli",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607555-009",
+					"dealerId": "35607555",
+					"tradingName": "RENAULT INDIRA NAGAR",
+					"geolocation": {
+						"latitude": 12.967039,
+						"longitude": 77.643763
+					},
+					"suggestedName": "RENAULT INDIRA NAGAR - BANGALORE",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607570-005",
+					"dealerId": "35607570",
+					"tradingName": "RENAULT JALANDHAR",
+					"geolocation": {
+						"latitude": 31.3223978,
+						"longitude": 75.5734192
+					},
+					"suggestedName": "RENAULT JALANDHAR - JALANDHAR",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607418-001",
+					"dealerId": "35607418",
+					"tradingName": "RENAULT JHANSI",
+					"geolocation": {
+						"latitude": 25.4791666,
+						"longitude": 78.5616111
+					},
+					"suggestedName": "RENAULT JHANSI - Jhansi",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607419-002",
+					"dealerId": "35607419",
+					"tradingName": "RENAULT JHANSI",
+					"geolocation": {
+						"latitude": 25.451194,
+						"longitude": 78.556972
+					},
+					"suggestedName": "RENAULT JHANSI - Jhansi",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607560-002",
+					"dealerId": "35607560",
+					"tradingName": "RENAULT JIND",
+					"geolocation": {
+						"latitude": 29.2900504,
+						"longitude": 76.3159826
+					},
+					"suggestedName": "RENAULT JIND - JIND",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_20005957-001",
+					"dealerId": "20005957",
+					"tradingName": "RENAULT Jaipur South",
+					"geolocation": {
+						"latitude": 26.843985,
+						"longitude": 75.802338
+					},
+					"suggestedName": "RENAULT Jaipur South - JAIPUR",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95002821-004",
+					"dealerId": "95002821",
+					"tradingName": "RENAULT Jodhpur",
+					"geolocation": {
+						"latitude": 26.257869,
+						"longitude": 73.006548
+					},
+					"suggestedName": "RENAULT Jodhpur - Jodhpur",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95002822-005",
+					"dealerId": "95002822",
+					"tradingName": "RENAULT Jodhpur",
+					"geolocation": {
+						"latitude": 26.23895,
+						"longitude": 73.024309
+					},
+					"suggestedName": "RENAULT Jodhpur - Jodhpur",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607559-001",
+					"dealerId": "35607559",
+					"tradingName": "RENAULT KAITHAL",
+					"geolocation": {
+						"latitude": 29.8220387,
+						"longitude": 76.4177884
+					},
+					"suggestedName": "RENAULT KAITHAL - KAITHAL",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607556-010",
+					"dealerId": "35607556",
+					"tradingName": "RENAULT KALYAN NAGAR (BS)",
+					"geolocation": {
+						"latitude": 13.025489,
+						"longitude": 77.642026
+					},
+					"suggestedName": "RENAULT KALYAN NAGAR (BS) - BANGALORE",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_35607577-003",
+					"dealerId": "35607577",
+					"tradingName": "RENAULT KRISHNANAGAR",
+					"geolocation": {
+						"latitude": 0.0,
+						"longitude": 0.0
+					},
+					"suggestedName": "RENAULT KRISHNANAGAR - KRISHNANAGAR",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95003160-002",
+					"dealerId": "95003160",
+					"tradingName": "RENAULT Kanpur Service",
+					"geolocation": {
+						"latitude": 26.434956,
+						"longitude": 80.17717
+					},
+					"suggestedName": "RENAULT Kanpur Service - Kanpur",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_95000500-001",
+					"dealerId": "95000500",
+					"tradingName": "RENAULT Karnal",
+					"geolocation": {
+						"latitude": 29.69029,
+						"longitude": 77.00592
+					},
+					"suggestedName": "RENAULT Karnal - Karnal",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_20005959-001",
+					"dealerId": "20005959",
+					"tradingName": "RENAULT Kochi",
+					"geolocation": {
+						"latitude": 10.05068,
+						"longitude": 76.32015
+					},
+					"suggestedName": "RENAULT Kochi - Kochi",
+					"markerStyle": ""
+				}, {
+					"id": "in_renault_20005965-003",
+					"dealerId": "20005965",
+					"tradingName": "RENAULT L B Nagar",
+					"geolocation": {
+						"latitude": 17.35713,
+						"longitude": 78.556847
+					},
+					"suggestedName": "RENAULT L B Nagar - L B Nagar Muncip…
